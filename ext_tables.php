8467439bb54ac4847e8d086b02968dd1685b570a
Christopher Kaster <me@atomicptr.de>
2017-09-10 18:41:46 +0200

    also add icon to plugins

--- ext_tables.php
+++ ext_tables.php
@@ -6,13 +6,15 @@ call_user_func(
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
             "Atomicptr.Blogging",
             "PostListPlugin",
-            "List posts - Blogging"
+            "List posts - Blogging",
+            "EXT:blogging/ext_icon.png"
         );
 
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
             "Atomicptr.Blogging",
             "CategoryPostListPlugin",
-            "List Posts by Category - Blogging"
+            "List Posts by Category - Blogging",
+            "EXT:blogging/ext_icon.png"
         );
 
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, "Configuration/TypoScript", "Blogging");
